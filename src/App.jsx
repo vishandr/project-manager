557c41bd0c5764538f0e4c999d74f9ce0695aba0
vishandr <vishandr@gmail.com>
2023-11-07 15:24:38 +0200

    add Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Sidebar from './components/Sidebar';
+// import MainSection from './MainSection';
+
 export default function App() {
   return (
-    <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-  )
+    <main className='h-screen my-8 flex gap-8'>
+      <Sidebar />
+      {/* <MainSection /> */}
+    </main>
+  );
 }
