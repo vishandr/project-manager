3b9a477ee9e79cf03d330d8bf64e608d280ac41c
vishandr <vishandr@gmail.com>
2023-11-07 16:08:14 +0200

    add styles to Input and NewProject components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from './components/Sidebar';
+import NewProject from './components/NewProject';
 // import MainSection from './MainSection';
 
 export default function App() {
   return (
     <main className='h-screen my-8 flex gap-8'>
       <Sidebar />
+      <NewProject />
       {/* <MainSection /> */}
     </main>
   );
