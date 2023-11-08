ac8877cae4f99f03761b9a93be8ea2fbbf4d28a1
vishandr <vishandr@gmail.com>
2023-11-08 09:51:06 +0200

    add Button and NoProject component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import {useState} from 'react';
 import Sidebar from './components/Sidebar';
 import NewProject from './components/NewProject';
-// import MainSection from './MainSection';
+import NoProjectSelected from './components/NoProjectSelected';
 
 export default function App() {
+  const [addingNewProject, setAddingNewProject] = useState(false)
+
+  const handleAddNewProject = () => {
+    setAddingNewProject(true)
+  };
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <Sidebar />
-      <NewProject />
+      <Sidebar onAddNewProject={handleAddNewProject}/>
+      {addingNewProject && <NewProject />}
+      {!addingNewProject && <NoProjectSelected />}
       {/* <MainSection /> */}
     </main>
   );
