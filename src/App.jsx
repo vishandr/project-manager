c4ba8eaa265f22dcbcb8371c13701c4381895dc0
vishandr <vishandr@gmail.com>
2023-11-08 09:56:47 +0200

    add functionality to Cancel button in NewProject component

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 export default function App() {
-  const [addingNewProject, setAddingNewProject] = useState(false)
+  const [isAddingNewProject, setIsAddingNewProject] = useState(false)
 
   const handleAddNewProject = () => {
-    setAddingNewProject(true)
+    setIsAddingNewProject(true)
   };
+
+  const handleCancelNewProject = () => {
+    setIsAddingNewProject(false)
+  }
+
   return (
     <main className='h-screen my-8 flex gap-8'>
       <Sidebar onAddNewProject={handleAddNewProject}/>
-      {addingNewProject && <NewProject />}
-      {!addingNewProject && <NoProjectSelected />}
-      {/* <MainSection /> */}
+      {isAddingNewProject && <NewProject onCancel={handleCancelNewProject}  />}
+      {!isAddingNewProject && <NoProjectSelected />}
     </main>
   );
 }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,11 +1,11 @@
 import Input from './Input';
 
-const NewProject = () => {
+const NewProject = ({onCancel}) => {
   return (
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4'>
         <li>
-          <button className='text-stone-800 hover:text-stone-950'>Cancel</button>
+          <button onClick={onCancel} className='text-stone-800 hover:text-stone-950'>Cancel</button>
         </li>
         <li>
           <button className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md'>Save</button>
