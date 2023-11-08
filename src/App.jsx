176aeef36ced0c1715d424717bd5c4b22261b414
vishandr <vishandr@gmail.com>
2023-11-08 12:21:05 +0200

    add passing data from AddNew to State

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 export default function App() {
-  const [isAddingNewProject, setIsAddingNewProject] = useState(false)
-
-  const [projectState, setProjectState] = useState({
+    const [projectState, setProjectState] = useState({
     selectedProjectId: undefined,
     projects: []
   })  
@@ -19,9 +17,6 @@ export default function App() {
       }
     })
   };
-  // const handleAddNewProject = () => {
-  //   setIsAddingNewProject(true)
-  // };
 
   const handleCancelNewProject = () => {
     setProjectState(prevState => {
@@ -32,11 +27,28 @@ export default function App() {
     })
   };
   
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const id = prevState.projects.length === 0 ? 1 : prevState.projects[0].id + 1;
+
+      const newProject = {
+        ...projectData,
+        id: id
+      }
+      
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+        // selectedProjectId: project.id
+      }
+    })
+  }
+  console.log(projectState)
 
   return (
     <main className='h-screen my-8 flex gap-8'>
       <Sidebar onAddNewProject={handleAddNewProject}/>
-      {projectState.selectedProjectId === null && <NewProject onCancel={handleCancelNewProject}  />}
+      {projectState.selectedProjectId === null && <NewProject onSave={handleAddProject} onCancel={handleCancelNewProject}  />}
       {projectState.selectedProjectId === undefined && <NoProjectSelected onAddNewProject={handleAddNewProject}/>}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,25 +1,45 @@
-import { useRef } from "react";
+import { useRef } from 'react';
 import Input from './Input';
 
-const NewProject = ({onCancel}) => {
+const NewProject = ({ onCancel, onSave }) => {
   const title = useRef();
   const description = useRef();
-  const dueDate  = useRef();
+  const dueDate = useRef();
+
+  function handleSubmit() {
+    const data = {
+      title: title.current.value,
+      description: description.current.value,
+      dueDate: dueDate.current.value
+    }
+    onSave(data);
+    onCancel();
+  }
 
   return (
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4'>
         <li>
-          <button onClick={onCancel} className='text-stone-800 hover:text-stone-950'>Cancel</button>
+          <button
+            onClick={onCancel}
+            className='text-stone-800 hover:text-stone-950'
+          >
+            Cancel
+          </button>
         </li>
         <li>
-          <button className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md'>Save</button>
+          <button
+            onClick={handleSubmit}
+            className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md'
+          >
+            Save
+          </button>
         </li>
       </menu>
       <div>
-        <Input ref={title} label='Title' />
+        <Input type='text' ref={title} label='Title' />
         <Input ref={description} label='Description' textarea />
-        <Input ref={dueDate} label='Due Date' />
+        <Input type='date' ref={dueDate} label='Due Date' />
       </div>
     </div>
   );
